de57741956d2df4f1e377384c2a83dd3bb1e1805
KaluMiracle <60604821+KaluMiracle@users.noreply.github.com>
2022-04-20 23:01:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { Draggable, Map, Marker } from "pigeon-maps"
+import { Autocomplete, TextField } from '@mui/material';
+import cities from './cities.json'
+import { InputAdornment } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { OnlinePredictionSharp } from '@mui/icons-material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [options, setOptions] = useState( cities);
+   const [anchor, setAnchor] = useState([50.879, 4.6997]);
+
+
+  
+
+
+
+
+
+
+   const mapRef = useRef()
+   const handleclick = ()=>{
+     console.log(mapRef.current)
+   }
+  
+
+   
+  useEffect(()=>{
+    console.log(anchor)
+  })
+
+   
+  return(
+    <div>
+      <head>
+        <meta charset="UTF-8"></meta>
+      </head>
+      <Map ref={mapRef} height={300} center={anchor} defaultZoom={5}>
+        <Draggable  offset={[60, 87]} anchor={anchor} onDragEnd={setAnchor}>
+          <img src="./logo.svg" width={100} height={95} alt="Pigeon!" />
+        </Draggable>
+      </Map>
+
+
+
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={options}
+        sx={{ width: 300 }}
+        getOptionLabel={(option) => `${option.name}`}
+        
+        renderInput={(params) => <TextField 
+            {...params}  
+            label="Countries" 
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <img src={`https://openweathermap.org/img/wn/${params.emoji}@2x.png`} alt={params.emoji}/>
+                </InputAdornment>
+              )
+            }}
+
+           
+          />}
+        onChange={(option, newValue)=>setAnchor(newValue ? [parseFloat(newValue.latitude), parseFloat(newValue.longitude)] : [0,0] )}
+      />
+
     </div>
-  );
+  )
+   
 }
 
 export default App;
