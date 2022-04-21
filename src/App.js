bfe95caf8a2e2077c57ef1cac0386134db4b0ef4
KaluMiracle <60604821+KaluMiracle@users.noreply.github.com>
2022-04-21 13:02:48 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,70 +1,60 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Draggable, Map, Marker } from "pigeon-maps"
+import React, { useEffect, useState } from 'react';
+import { Draggable, Map,  } from "pigeon-maps"
 import { Autocomplete, TextField } from '@mui/material';
-import cities from './cities.json'
-import { InputAdornment } from '@mui/material';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import { OnlinePredictionSharp } from '@mui/icons-material';
+import countries from './countries.json'
+
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  const [options, setOptions] = useState( cities);
-   const [anchor, setAnchor] = useState([50.879, 4.6997]);
+   const [anchor, setAnchor] = useState([
+     parseFloat(countries[0].latitude), parseFloat(countries[0].longitude)
+    ]);
 
-
-  
-
-
-
-
-
-
-   const mapRef = useRef()
-   const handleclick = ()=>{
-     console.log(mapRef.current)
-   }
-  
-
-   
-  useEffect(()=>{
-    console.log(anchor)
-  })
-
-   
+   useEffect(()=>{
+     console.log(anchor)
+   })
   return(
-    <div>
-      <head>
-        <meta charset="UTF-8"></meta>
-      </head>
-      <Map ref={mapRef} height={300} center={anchor} defaultZoom={5}>
-        <Draggable  offset={[60, 87]} anchor={anchor} onDragEnd={setAnchor}>
-          <img src="./logo.svg" width={100} height={95} alt="Pigeon!" />
+    <div className='app'>
+      
+      <Map  width={'100%'} height={'50vh'} center={anchor} defaultZoom={3}>
+        <Draggable   anchor={anchor} onDragEnd={setAnchor}>
+          <div className='marker'>
+            <img src={logo} alt="" />
+            <p>lat: {parseFloat(anchor[0]).toPrecision(4)}</p>
+            <p>lng: {parseFloat(anchor[1]).toPrecision(4)}</p>
+          </div>
+          
         </Draggable>
       </Map>
 
+      <form className='container'>
+        
 
+      <h4>Enter latitude and logitude to show on the Map</h4>
 
+      <input className='input' placeholder={`latitude: ${anchor[0]}`} type={'number'} onChange={(e)=> setAnchor([parseFloat(e.target.value), anchor[1]])}/>
+      <input className='input' placeholder={`longitude: ${anchor[1]}`} type={'number'} onChange={(e)=> setAnchor([anchor[0], parseFloat(e.target.value)])}/>
+
+      <h4>Or Enter Country to show on the Map</h4>
       <Autocomplete
         disablePortal
         id="combo-box-demo"
-        options={options}
-        sx={{ width: 300 }}
-        getOptionLabel={(option) => `${option.name}`}
-        
+        options={countries}
+        sx={{ width: 300, }}
+        getOptionLabel={(option) => `${option.name}  ${option.emoji}`}
+        defaultValue={countries[0]}
         renderInput={(params) => <TextField 
             {...params}  
             label="Countries" 
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <img src={`https://openweathermap.org/img/wn/${params.emoji}@2x.png`} alt={params.emoji}/>
-                </InputAdornment>
-              )
-            }}
+            
 
            
           />}
-        onChange={(option, newValue)=>setAnchor(newValue ? [parseFloat(newValue.latitude), parseFloat(newValue.longitude)] : [0,0] )}
+        onChange={(params, newValue)=>setAnchor(newValue.latitude ? [parseFloat(newValue.latitude).toPrecision(4), parseFloat(newValue.longitude).toPrecision(4)] : [0,0] )}
       />
+
+      </form>
 
     </div>
   )
