1b0f07a0a914630fb863dc68a2f1bcac2db54530
KaluMiracle <60604821+KaluMiracle@users.noreply.github.com>
2022-04-21 13:08:31 +0100

    remove unnecessary dependencies

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
      parseFloat(countries[0].latitude), parseFloat(countries[0].longitude)
     ]);
 
-   useEffect(()=>{
-     console.log(anchor)
-   })
+   
   return(
     <div className='app'>
       
